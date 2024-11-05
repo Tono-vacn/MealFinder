bcac4ce9baa24734d7aebadfd5e54061750c82ca
Seochyeob <sy>
2024-11-05 10:52:35 -0500

    add detail page

--- mealFinder/mealFinder/Views/DetailView.swift
+++ mealFinder/mealFinder/Views/DetailView.swift
@@ -2,15 +2,78 @@
 //  DetailView.swift
 //  mealFinder
 //
-//  Created by Shujie on 10/25/24.
+//  Created by Shujie on 10/22/24.
 //
 
 import SwiftUI
 
 struct DetailView: View {
+    let recipe: Recipe
+    
     var body: some View {
-        Text("Detail View")
-            .font(.largeTitle)
-            .padding()
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+
+                    Text(recipe.title)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.top)
+                    
+
+                    AsyncImage(url: URL(string: recipe.image)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(height: 300)
+                    .cornerRadius(10)
+                    .padding(.vertical)
+                }
+                .padding()
+            }
+            
+
+            Spacer()
+            
+
+            Button(action: saveRecipe) {
+                HStack {
+                    Image(systemName: "bookmark.fill")
+                    Text("Save")
+                        .fontWeight(.bold)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.horizontal)
+                .padding(.bottom)
+            }
+        }
+        .navigationTitle("Cooking steps")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: shareRecipe) {
+                    Image(systemName: "square.and.arrow.up")
+                        .font(.title2)
+                        .foregroundColor(.blue)
+                }
+            }
+        }
+    }
+    
+
+    func saveRecipe() {
+        print("Recipe saved!")
+    }
+    
+    func shareRecipe() {
+        print("Recipe shared!")
     }
 }
--- mealFinder/mealFinder/Views/RecipePopUpView.swift
+++ mealFinder/mealFinder/Views/RecipePopUpView.swift
@@ -13,111 +13,118 @@ struct RecipePopUpView: View {
     let onNext: () -> Void
     let onClose: () -> Void
     
-    @State private var navigateToDetail = false
     
     var body: some View {
-        VStack {
-            
-            HStack {
-                Spacer()
-                Button(action: onClose) {
-                    Image(systemName: "xmark.circle.fill")
-                        .font(.largeTitle)
-                        .foregroundColor(.gray)
-                }
-                .padding()
-            }
-            
-            
-            Text(recipe.title)
-                .font(.title)
-                .padding()
-            
-            
-            AsyncImage(url: URL(string: recipe.image)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(height: 150)
-            .padding()
-            
-            Spacer()
-            
-            
-            ScrollView {
-                VStack(alignment: .leading, spacing: 10) {
-                    if !recipe.usedIngredients.isEmpty {
-                        Text("Used Ingredients:")
-                            .font(.subheadline)
-                            .padding(.bottom, 5)
-                        
-                        ForEach(recipe.usedIngredients, id: \.id) { ingredient in
-                            Text("• \(ingredient.original)")
-                                .font(.body)
+        
+        
+        NavigationView{
+            NavigationLink(destination: DetailView(recipe: recipe)){
+                VStack {
+                    
+                    HStack {
+                        Spacer()
+                        Button(action: onClose) {
+                            Image(systemName: "xmark.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.gray)
                         }
+                        .padding()
                     }
                     
-                    if !recipe.missedIngredients.isEmpty {
-                        Text("Missed Ingredients:")
-                            .font(.subheadline)
-                            .padding(.top, 10)
-                        
-                        ForEach(recipe.missedIngredients, id: \.id) { ingredient in
-                            Text("• \(ingredient.original)")
-                                .font(.body)
-                                .foregroundColor(.red)
-                        }
+                    
+                    Text(recipe.title)
+                        .font(.title)
+                        .foregroundColor(.black) 
+                        .padding()
+                    
+                    
+                    AsyncImage(url: URL(string: recipe.image)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                        ProgressView()
                     }
-                }
-                .padding(.horizontal)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .frame(maxWidth: .infinity)
-            
-            GeometryReader { geometry in
-                HStack {
-                    Button(action: onPrevious) {
-                        Image(systemName: "chevron.left")
-                            .font(.largeTitle)
-                            .foregroundColor(.black)
-                            .background(
-                                Circle()
-                                    .fill(Color.black.opacity(0))
-                                    .frame(width: 30, height: 30)
-                            )
-                    }.padding(10)
+                    .frame(height: 150)
+                    .padding()
                     
                     Spacer()
                     
                     
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 10) {
+                            if !recipe.usedIngredients.isEmpty {
+                                Text("Used Ingredients:")
+                                    .font(.subheadline)
+                                    .foregroundColor(.black)
+                                    .padding(.bottom, 5)
+                                
+                                ForEach(recipe.usedIngredients, id: \.id) { ingredient in
+                                    Text("• \(ingredient.original)")
+                                        .font(.body)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            
+                            if !recipe.missedIngredients.isEmpty {
+                                Text("Missed Ingredients:")
+                                    .font(.subheadline)
+                                    .padding(.top, 10)
+                                    .foregroundColor(.black)
+                                
+                                ForEach(recipe.missedIngredients, id: \.id) { ingredient in
+                                    Text("• \(ingredient.original)")
+                                        .font(.body)
+                                        .foregroundColor(.red)
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(maxWidth: .infinity)
                     
-                    Button(action: onNext) {
-                        Image(systemName: "chevron.right")
-                            .font(.largeTitle)
-                            .foregroundColor(.black)
-                            .background(
-                                Circle()
-                                    .fill(Color.white.opacity(0))
-                                    .frame(width: 30, height: 30)
-                            )
-                    }.padding(10)
-                    
-                }
-                .frame(height: 80)
-            }
-            .frame(height: 100)
-        }
-        .frame(width: 370, height: 700)
-        .background(Color.white)
-        .cornerRadius(20)
-        .shadow(radius: 10)
-        .overlay(
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(Color.gray, lineWidth: 1)
-        )
-        .padding()
+                    HStack {
+                        Button(action: onPrevious) {
+                            Image(systemName: "chevron.left")
+                                .font(.largeTitle)
+                                .foregroundColor(.black)
+                                .background(
+                                    Circle()
+                                        .fill(Color.white)
+                                        .frame(width: 50, height: 50)
+                                    //                                    .shadow(radius: 5)
+                                )
+                        }
+                        .padding(10)
+                        
+                        Spacer()
+                        
+                        Button(action: onNext) {
+                            Image(systemName: "chevron.right")
+                                .font(.largeTitle)
+                                .foregroundColor(.black)
+                                .background(
+                                    Circle()
+                                        .fill(Color.white)
+                                        .frame(width: 50, height: 50)
+                                    //                                    .shadow(radius: 5)
+                                )
+                        }
+                        .padding(10)
+                    }
+                    .frame(height: 100)
+                }}
+        }.frame(width: 370, height: 700)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+            .padding()
+        
+        
     }
 }
